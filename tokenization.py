5e252eb8b145946aeea4eb086ab0ab53e845892f
27-ganesh-07 <74642630+27-ganesh-07@users.noreply.github.com>
2022-02-26 21:17:38 +0530

    Add files via upload

---
+++ tokenization.py
@@ -0,0 +1,58 @@
+from util import *
+import re
+from nltk.tokenize import TreebankWordTokenizer
+# Add your import statements here
+
+
+
+
+class Tokenization():
+
+	def naive(self, text):
+		"""
+		Tokenization using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		#Fill in code here
+		tokenizedText = text.split()
+		tokenizedText = tokenizedText[:len(tokenizedText)-1]
+
+		return tokenizedText
+
+
+
+	def pennTreeBank(self, text):
+		"""
+		Tokenization using the Penn Tree Bank Tokenizer
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		#Fill in code here
+		tokenizedText = reebankWordTokenizer().tokenize(text)
+		tokenizedText = tokenizedText[:len(tokenizedText)-1]
+		
+
+		return tokenizedText
